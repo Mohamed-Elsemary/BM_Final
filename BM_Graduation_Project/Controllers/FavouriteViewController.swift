26767a12e4b47f1314cb7f9e750db56167d80dff
Mohamed-Elsemary <mohamedelsemary19@yahoo.com>
2023-08-29 20:03:02 +0300

    Final

--- BM_Graduation_Project/Controllers/FavouriteViewController.swift
+++ BM_Graduation_Project/Controllers/FavouriteViewController.swift
@@ -36,14 +36,6 @@ class FavouriteViewController: UIViewController {
 extension FavouriteViewController :settingTableView {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cells.outsideFavouriteCell, for: indexPath) as! outsideFavouriteCellTableViewCell
-         cell.checkBtn.isChecked = //is item
-            cell.checkbuttonPressed = {
-            if cell.checkBtn.isChecked {
-                // add
-            } else {
-                // remove
-            }
-        }
         cell.currN.text = favArr[indexPath.row].name
         cell.currencyName.text = favArr[indexPath.row].code
         let image = favArr[indexPath.row].flagURL
